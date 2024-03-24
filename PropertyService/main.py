6a6e0bdfe76b71c919eb99f07cf3bbfbb7e4adf5
Migas77 <miguel.belchior@ua.pt>
2024-03-24 19:36:07 +0000

    PTSE-37 propertys by user_id

--- PropertyService/main.py
+++ PropertyService/main.py
@@ -7,26 +7,21 @@ from PropertyService.schemas import Property
 app = FastAPI()
 
 
-@app.post("/properties", response_model=Property, status_code=status.HTTP_201_CREATED)
-async def create_property(prop: Property):
-    result = await collection.insert_one(prop.model_dump(exclude={"id"}))
-    return prop
+@app.get("/properties", response_model=list[Property])
+async def read_properties(user_id: int = None):
+    return await collection.find({} if user_id is None else {'user_id': user_id}).to_list(1000)
 
 
 @app.get("/properties/{prop_id}", response_model=Property, response_model_by_alias=False)
 async def read_property(prop_id: str):
     if not ObjectId.is_valid(prop_id) or (result := await collection.find_one({"_id": ObjectId(prop_id)})) is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Property {prop_id} not found")
-    print("Here2", result)
     return result
 
 
-@app.get("/properties", response_model=list[Property])
-async def read_properties():
-    return await collection.find().to_list(1000)
-
-
-"""
-response_model_include & response_model_exclude can be helpfull for later
-"""
-
+@app.post("/properties", response_model=Property, response_model_by_alias=False, status_code=status.HTTP_201_CREATED)
+async def create_property(prop: Property):
+    property_dict = prop.model_dump(exclude={"id"})
+    created_property = await collection.insert_one(property_dict)
+    # property_dict automatically gets id after insertion
+    return property_dict
