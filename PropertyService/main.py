546e30c72cc8cb4535645f5704e0fc20a392fff4
Migas77 <miguel.belchior@ua.pt>
2024-04-13 17:36:50 +0100

    separate health and application router

--- PropertyService/main.py
+++ PropertyService/main.py
@@ -1,6 +1,6 @@
 import firebase_admin
 from bson import ObjectId
-from fastapi import FastAPI, HTTPException, status, Depends
+from fastapi import FastAPI, HTTPException, status, Depends, APIRouter
 from firebase_admin import credentials
 from pymongo import ReturnDocument
 
@@ -23,21 +23,27 @@ async def lifespan(app: FastAPI):
 
 cred = credentials.Certificate(".secret.json")
 firebase_admin.initialize_app(cred)
-app = FastAPI(lifespan=lifespan, dependencies=[Depends(get_user)])
+app = FastAPI(lifespan=lifespan)
+healthRouter = APIRouter()
+appRouter = APIRouter(dependencies=[Depends(get_user)])
+app.include_router(healthRouter, tags=["health"])
+app.include_router(appRouter, tags=["application"])
 
-@app.get( "/health", tags=["healthcheck"], summary="Perform a Health Check", response_description="Return HTTP Status Code 200 (OK)", status_code=status.HTTP_200_OK)
+
+@healthRouter.get("/health", tags=["healthcheck"], summary="Perform a Health Check",
+               response_description="Return HTTP Status Code 200 (OK)", status_code=status.HTTP_200_OK)
 def get_health():
     return {"status": "ok"}
 
 
-@app.get("/properties", response_model=list[Property])
+@appRouter.get("/properties", response_model=list[Property])
 async def read_properties(user_id: int = None):
     return await collection.find(
         {} if user_id is None else {"user_id": user_id}
     ).to_list(1000)
 
 
-@app.post("/properties", response_model=Property, response_model_by_alias=False, status_code=status.HTTP_201_CREATED)
+@appRouter.post("/properties", response_model=Property, response_model_by_alias=False, status_code=status.HTTP_201_CREATED)
 async def create_property(prop: Property):
     property_dict = prop.model_dump(exclude={"id"})
     await collection.insert_one(property_dict)
@@ -45,14 +51,14 @@ async def create_property(prop: Property):
     return property_dict
 
 
-@app.get("/properties/{prop_id}", response_model=Property, response_model_by_alias=False)
+@appRouter.get("/properties/{prop_id}", response_model=Property, response_model_by_alias=False)
 async def read_property(prop_id: str):
     if not ObjectId.is_valid(prop_id) or (result := await collection.find_one({"_id": ObjectId(prop_id)})) is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Property {prop_id} not found")
     return result
 
 
-@app.put("/properties/{prop_id}", response_model=Property, response_model_by_alias=False)
+@appRouter.put("/properties/{prop_id}", response_model=Property, response_model_by_alias=False)
 async def update_property(prop_id: str, prop: UpdateProperty):
     upd_prop = {k: v for k, v in prop.model_dump().items() if v is not None}
 
@@ -61,24 +67,23 @@ async def update_property(prop_id: str, prop: UpdateProperty):
         return await read_property(prop_id)
 
     if (not ObjectId.is_valid(prop_id)
-        or (
-            update_result := await collection.find_one_and_update(
-                {"_id": ObjectId(prop_id)},
-                {"$set": upd_prop},
-                return_document=ReturnDocument.AFTER,
+            or (
+                    update_result := await collection.find_one_and_update(
+                        {"_id": ObjectId(prop_id)},
+                        {"$set": upd_prop},
+                        return_document=ReturnDocument.AFTER,
+                    )
             )
-        )
-        is None
+            is None
     ):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Property {prop_id} not found")
     return update_result
 
 
-@app.delete("/properties/{prop_id}", status_code=status.HTTP_204_NO_CONTENT)
+@appRouter.delete("/properties/{prop_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_property(prop_id: str):
     if (
-        not ObjectId.is_valid(prop_id)
-        or (await collection.delete_one({"_id": ObjectId(prop_id)})).deleted_count != 1
+            not ObjectId.is_valid(prop_id)
+            or (await collection.delete_one({"_id": ObjectId(prop_id)})).deleted_count != 1
     ):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Property {prop_id} not found")
-    
