c31b1296e40ffd071069f4f551968a028b4478e5
Diana <dianarrmiranda@gmail.com>
2024-04-13 18:05:36 +0100

    update database.py

--- PropertyService/database.py
+++ PropertyService/database.py
@@ -1,6 +1,13 @@
 from motor.motor_asyncio import AsyncIOMotorClient
+import os
+from dotenv import load_dotenv
 
-MONGO_DATABASE_URL = "mongodb://property_service_db:27017"
+load_dotenv()
+
+user = os.getenv("MONGO_INITDB_ROOT_USERNAME")
+password = os.getenv("MONGO_INITDB_ROOT_PASSWORD")
+
+MONGO_DATABASE_URL = f"mongodb://{user}:{password}@property_service_db:27017"
 
 client = AsyncIOMotorClient(MONGO_DATABASE_URL)
 database = client["mydatabase"]
