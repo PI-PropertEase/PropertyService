d22c2be6939484a2d0dd6d21800dc38137c360c7
notjoao1 <joao.dourado1@ua.pt>
2024-04-13 15:16:03 +0100

    PTSE-51 import properties from external services

--- PropertyService/messaging_operations.py
+++ PropertyService/messaging_operations.py
@@ -5,7 +5,11 @@ from ProjectUtils.MessagingService.queue_definitions import (
     USER_QUEUE_NAME,
     EXCHANGE_NAME,
     USER_QUEUE_ROUTING_KEY,
+    WRAPPER_TO_APP_QUEUE,
+    WRAPPER_TO_APP_ROUTING_KEY,
 )
+from ProjectUtils.MessagingService.schemas import MessageType
+from PropertyService.database import collection
 
 # TODO: fix this in the future
 channel.close()  # don't use the channel from this file, we need to use an async channel
@@ -16,15 +20,50 @@ async def consume(loop):
 
     channel = await connection.channel()
 
-    queue = await channel.declare_queue(USER_QUEUE_NAME, durable=True)
+    users_queue = await channel.declare_queue(USER_QUEUE_NAME, durable=True)
 
-    await queue.bind(exchange=EXCHANGE_NAME, routing_key=USER_QUEUE_ROUTING_KEY)
+    wrappers_queue = await channel.declare_queue(WRAPPER_TO_APP_QUEUE, durable=True)
 
-    await queue.consume(callback=consume_message)
+    await users_queue.bind(exchange=EXCHANGE_NAME, routing_key=USER_QUEUE_ROUTING_KEY)
+
+    await wrappers_queue.bind(
+        exchange=EXCHANGE_NAME, routing_key=WRAPPER_TO_APP_ROUTING_KEY
+    )
+
+    await users_queue.consume(callback=consume_user_message)
+
+    await wrappers_queue.consume(callback=consume_wrappers_message)
 
     return connection
 
 
-async def consume_message(message):
-    async with message.process():
-        print(message.body)
+async def consume_user_message(incoming_message):
+    print("Received Message @ Users queue")
+    async with incoming_message.process():
+        try:
+            decoded_message = json.loads(incoming_message)
+        except Exception as e:
+            print("Error while processing message:", e)
+        print(incoming_message.body)
+
+
+async def consume_wrappers_message(incoming_message):
+    print("Received Message @ Wrappers queue")
+    async with incoming_message.process():
+        try:
+            decoded_message = json.loads(incoming_message.body)
+            if decoded_message.get("type") == MessageType.PROPERTY_IMPORT_RESPONSE:
+                await import_properties(decoded_message.get("body"))
+        except Exception as e:
+            print("Error while processing message:", e)
+
+
+async def import_properties(properties):
+    print("IMPORT_PROPERTIES_RESPONSE - importing properties...")
+    for prop in properties:
+        num_prop_same_address = await collection.count_documents(
+            {"address": prop.get("address"), "user_email": prop.get("user_email")}
+        )
+        if num_prop_same_address > 0:
+            continue  # don't import properties that already exist for this user
+        await collection.insert_one(prop)
